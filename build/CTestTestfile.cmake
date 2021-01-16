83ce954ec3342ade9a8baa21cf6a681d01a6b1ed
Kamil Kowalski <kamilkowalski314@gmail.com>
2021-01-16 17:04:31 +0100

    removed cmake trash

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kamil2/Desktop/Programming/C++/Exercises/BigInt5
-# Build directory: /home/kamil2/Desktop/Programming/C++/Exercises/BigInt5/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
